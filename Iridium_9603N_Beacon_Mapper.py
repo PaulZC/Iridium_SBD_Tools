7d4bbbb0c683f8c0110f38bf101959b9f0b2420b
Paul <pc235603@gmail.com>
2018-01-05 22:01:25 +0000

    Corrected scale multiplier

--- Iridium_9603N_Beacon_Mapper.py
+++ Iridium_9603N_Beacon_Mapper.py
@@ -435,10 +435,7 @@ class BeaconMapper(object):
          y_move = event.y - (self.frame_height / 2) # Required y move in pixels
          scale_x = self.scales[np.where(int(self.zoom)==self.scales[:,0])][0][1] # Select scale from scales using current zoom
          # Compensate y scale (Mercator projection) using current latitude
-         if abs(self.map_lat) > 1.: # Check for non-zero lat to avoid divide by zero error
-            scale_multiplier_lat = math.sin(abs(math.radians(self.map_lat))) / math.tan(abs(math.radians(self.map_lat)))
-         else:
-            scale_multiplier_lat = 1.0
+         scale_multiplier_lat = math.cos(math.radians(self.map_lat))
          scale_y = scale_x * scale_multiplier_lat # Calculate y scale
          new_lat = self.map_lat - (y_move * scale_y) # Calculate new latitude
          new_lon = self.map_lon + (x_move * scale_x) # Calculate new longitude
